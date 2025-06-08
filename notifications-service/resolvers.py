0acf98327cb8de3cf85358d93478a972d798b534
mchaz <mochammadaziiz02@gmail.com>
2025-06-08 15:18:24 +0700

    menambah crud notification

--- notifications-service/resolvers.py
+++ notifications-service/resolvers.py
@@ -25,3 +25,41 @@ def resolve_add_notification(_, info, user_id, item_id, message, status="pending
     db.refresh(notif)
     db.close()
     return notif
+
+@query.field("getNotificationById")
+def resolve_get_notification_by_id(_, info, id):
+    db = SessionLocal()
+    notif = db.query(Notification).filter(Notification.id == id).first()
+    db.close()
+    return notif
+
+@mutation.field("updateNotification")
+def resolve_update_notification(_, info, id, message=None, status=None):
+    db = SessionLocal()
+    notif = db.query(Notification).filter(Notification.id == id).first()
+    if not notif:
+        db.close()
+        return None
+    if message:
+        notif.message = message
+    if status:
+        notif.status = status
+    db.commit()
+    db.refresh(notif)
+    db.close()
+    return notif
+
+@mutation.field("deleteNotification")
+def resolve_delete_notification(_, info, id):
+    db = SessionLocal()
+    notif = db.query(Notification).filter(Notification.id == id).first()
+    if not notif:
+        db.close()
+        return False
+    db.delete(notif)
+    db.commit()
+    db.close()
+    return True
+
+    
+    
